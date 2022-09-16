f64a85fc746a25e50bd362fa50cea4d6480749ff
Hugo <gthugo@outlook.com>
2022-09-15 19:23:17 -0500

    Building the posts views

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 
 from .views import hello_world, numbers, hi
+
+from posts import views as posts_views
 
 
 urlpatterns = [
@@ -9,4 +11,6 @@ urlpatterns = [
     path('welcome', hello_world),
     path('numbers', numbers),
     path('hi/<str:name>/<int:age>', hi),
+
+    path('', posts_views.list_posts),
 ]
